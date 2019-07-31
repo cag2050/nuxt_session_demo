e90e8e3d88c5c83ea370e4690585344aadb765a3
chen_anguo <cag2050@163.com>
2019-07-31 17:25:50 +0800

    add: koa-session

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const Koa = require('koa')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
+const session = require('koa-session')
 
 const app = new Koa()
+
+// https://github.com/koajs/session#example
+const CONFIG = {
+  key: 'koa:session',
+  maxAge: 86400000,
+  overwrite: true,
+  httpOnly: true,
+  signed: true,
+  rolling: false
+}
+app.keys = ['some session']
+app.use(session(CONFIG, app))
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -26,10 +39,29 @@ async function start() {
   }
 
   app.use((ctx) => {
-    ctx.status = 200
-    ctx.respond = false // Bypass Koa's built-in response handling
-    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
-    nuxt.render(ctx.req, ctx.res)
+    // ctx.status = 200
+    // ctx.respond = false // Bypass Koa's built-in response handling
+    // ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    // // 设置session，自己添加；之后可以在 req 上拿到
+    // ctx.req.session = ctx.session
+    // // 用 Nuxt.js 渲染每个路由
+    // // 建议把 nuxt.render 放到中间件列表的最后面，因为它不会再调用 next() 方法，而是直接处理你 web 应用的页面渲染。
+    // nuxt.render(ctx.req, ctx.res)
+
+    // ignore favicon
+    // if (ctx.path === '/favicon.ico') return;
+    //
+    // let n = ctx.session.views || 0;
+    // ctx.session.views = ++n;
+    // ctx.body = n + ' views';
+
+    ctx.session.user = {
+      name: 'jeff',
+      age: 30
+    }
+    ctx.req.session = ctx.session
+    ctx.body = ctx
+    console.log(ctx.session)
   })
 
   app.listen(port, host)
